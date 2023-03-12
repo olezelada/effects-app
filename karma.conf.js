20a0e009262165cc9c7ecaea6e0d4908ecf85d34
ozelada <olegario.zelada@dharbor.com>
2023-03-12 19:27:06 -0400

    produccion listo

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/05-http-effects'),
+      dir: require('path').join(__dirname, './coverage/05-docs'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '05-http-effects';
+  title = '05-docs';
 }
